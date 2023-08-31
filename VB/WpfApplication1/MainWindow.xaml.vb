3db245906df4d3a28a24c25f0efbf24e7cc174c5
DevExpressExampleBot <fakeEmail>
2023-08-31 11:35:45 +0400

    Source auto update [skip ci]

--- VB/WpfApplication1/MainWindow.xaml.vb
+++ VB/WpfApplication1/MainWindow.xaml.vb
@@ -1,43 +1,46 @@
-﻿Imports System
+Imports System
 Imports System.Windows
 Imports DevExpress.XtraScheduler
 
 Namespace WpfApplication1
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
+    Public Partial Class MainWindow
+        Inherits Window
 
-'			#Region "#CustomWorkingDays "
-			schedulerControl1.WorkDays.BeginUpdate()
-			schedulerControl1.WorkDays.Clear()
-			schedulerControl1.WorkDays.Add(WeekDays.Monday Or WeekDays.Tuesday Or WeekDays.Wednesday Or WeekDays.Thursday)
-			schedulerControl1.WorkDays.EndUpdate()
-'			#End Region ' #CustomWorkingDays
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+#Region "#CustomWorkingDays "
+            Me.schedulerControl1.WorkDays.BeginUpdate()
+            Me.schedulerControl1.WorkDays.Clear()
+            Me.schedulerControl1.WorkDays.Add(WeekDays.Monday Or WeekDays.Tuesday Or WeekDays.Wednesday Or WeekDays.Thursday)
+            Me.schedulerControl1.WorkDays.EndUpdate()
+#End Region  ' #CustomWorkingDays
+        End Sub
+    End Class
 
-	#Region "#CustomTimeScale"
-	Public Class CustomTimeScale
-		Inherits TimeScaleFixedInterval
+#Region "#CustomTimeScale"
+    Public Class CustomTimeScale
+        Inherits TimeScaleFixedInterval
 
-		' Fields
-		Private Const defaultEnabled As Boolean = True
-		' Methods
-		Public Sub New()
-			MyBase.New(TimeSpan.FromHours(4.0))
-		End Sub
-		Protected Overrides ReadOnly Property DefaultDisplayName() As String
-			Get
-				Return "Custom Scale"
-			End Get
-		End Property
-		Protected Overrides ReadOnly Property DefaultMenuCaption() As String
-			Get
-				Return "Custom Scale"
-			End Get
-		End Property
-	End Class
-	#End Region ' #CustomTimeScale
+        ' Fields
+        Private Const defaultEnabled As Boolean = True
+
+        ' Methods
+        Public Sub New()
+            MyBase.New(TimeSpan.FromHours(4.0))
+        End Sub
+
+        Protected Overrides ReadOnly Property DefaultDisplayName As String
+            Get
+                Return "Custom Scale"
+            End Get
+        End Property
+
+        Protected Overrides ReadOnly Property DefaultMenuCaption As String
+            Get
+                Return "Custom Scale"
+            End Get
+        End Property
+    End Class
+#End Region  ' #CustomTimeScale
 End Namespace
